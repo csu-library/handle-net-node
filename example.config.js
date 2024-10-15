c5bf2e725af6c097b61c4c876822bcbb4e405291
Ed Hill <edhill@protonmail.com>
2024-10-15 15:12:30 -0700

    Remove DotEnv dependency
    Limit the number of packages. Also, for a project of this
    size just passing a config object is a more straightforward
    way of doing the configs.

---
+++ example.config.js
@@ -0,0 +1,10 @@
+export default {
+  "authHashAlgorithm": "SHA1",
+  "authID": "300:0.NA/10217",
+  "authPrivateKeyPath": "/admpriv.key",
+  "serverHost": "localhost",
+  "serverPort": "8000",
+  "serverPath": "/api",
+  "serverSelfSigned": "yes",
+  "testHandle": "10217/test"
+}
--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 import Axios from 'axios';
-import Dotenv from 'dotenv';
 import https from 'https';
 import Crypto from 'crypto';
 import fs from 'fs';
 
-Dotenv.config(0);
-
 class HandleNet {
-  hashAlgorithm = process.env.HANDLE_HASH_ALGORITHM || 'sha1';
-  authId = process.env.HANDLE_AUTH_ID || '300:0.NA/12345';
-  privateKeyPath = process.env.HANDLE_AUTH_PRIVATE_KEY_PATH || '/admpriv.key';
 
-  serverHost = process.env.HANDLE_SERVER_HOST || 'localhost';
-  serverPort = process.env.HANDLE_SERVER_PORT || '8000';
-  serverPath = process.env.HANDLE_SERVER_PATH || '/api';
-  serverSelfSigned = process.env.HANDLE_SERVER_SELF_SIGNED.toLowerCase() == 'yes';
-
-  testHandle = process.env.HANDLE_TEST_HANDLE || '12345/test';
-
-  sessionId = '';
-
-  constructor() {
-
+  constructor(config = {}) {
+    this.hashAlgorithm = config.hashAlgorithm || "sha1";
+    this.authId = config.authID || "300:0.NA/12345";
+    this.privateKeyPath = config.authPrivateKeyPath || "/admpriv.key";
+    this.serverHost = config.serverHost || "localhost";
+    this.serverPort = config.serverPort || "8000";
+    this.serverPath = config.serverPath || "/api";
+    this.serverSelfSigned = config.serverSelfSigned.toLowerCase() == 'yes';
+    this.testHandle = config.testHandle || "12345/test";
+    this.sessionId = "";
   }
 
   apiConfig() {
@@ -35,33 +28,26 @@ class HandleNet {
 
   async auth() {
     const handleAPI = Axios.create(this.apiConfig());
-  
+
     const key = fs.readFileSync(this.privateKeyPath);
-    
+
     let resUnauth = await handleAPI.post('/sessions/');
-  
+
     let sessionId = resUnauth.data.sessionId;
     let serverNonce = resUnauth.data.nonce;
     let serverBuff = Buffer.from(serverNonce, 'base64');
-    
+
     let clientBuff = await Crypto.randomBytes(16);
     let clientNonce = clientBuff.toString('base64');
-  
+
     let combinedBuff = Buffer.concat([serverBuff, clientBuff]);
     // let combinedNonce = combinedBuff.toString('base64');
-  
+
     let signatureBuff = Crypto.sign(this.hashAlgorithm, combinedBuff, key);
     let signatureNonce = signatureBuff.toString('base64');
-  
+
     let authString = `Handle version="0", sessionId="${sessionId}", cnonce="${clientNonce}", id="${this.authId}", type="HS_PUBKEY", alg="${this.hashAlgorithm}", signature="${signatureNonce}"`;
-    
-    // console.log(`SESSION ID: ${sessionId}`);
-    // console.log(`SERVER NONCE: ${serverNonce}`);
-    // console.log(`CLIENT NONCE: ${clientNonce}`);
-    // console.log(`COMBINED NONCE: ${combinedNonce}`);
-    // console.log(`SIGNATURE NONCE: ${signatureNonce}`);
-    // console.log(`AUTH STRING: ${authString}`);
-  
+
     let resAuth = await handleAPI.post('/sessions/this', null, {
       headers: {
         'Authorization': authString
@@ -69,7 +55,7 @@ class HandleNet {
     });
 
     this.sessionId = sessionId;
-  
+
     if (!resAuth.data.authenticated) {
       throw new Error('Handle server responded, but did not authenticate');
     }
@@ -86,20 +72,20 @@ class HandleNet {
         'Authorization': `Handle version="0", sessionId="${this.sessionId}"`
       }
     });
-  
+
     let api = Axios.create(reqConfig);
-  
+
     return api.request(reqConfig);
   }
 
   async get(path) {
     return this.req('get', path, null);
   }
-  
+
   async put(path, body) {
     return this.req('put', path, body);
   }
-  
+
   async del(path, body) {
     return this.req('delete', path, body);
   }
@@ -107,19 +93,19 @@ class HandleNet {
   async getHandles(prefix) {
     return this.get(`/handles?prefix=${prefix}`);
   }
-  
+
   async getHandle(handle) {
     return this.get(`/handles/${handle}`);
   }
-  
+
   async createHandle(handle, data) {
     return this.put(`/handles/${handle}?overwrite=false`, data);
   }
-  
+
   async updateHandle(handle, data) {
     return this.put(`/handles/${handle}?overwrite=true`, data);
   }
-  
+
   async deleteHandle(handle) {
     return this.del(`/handles/${handle}`);
   }
